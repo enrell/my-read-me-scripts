9c75b73b5db5f34ab2b834f132a0a46133832ae0
enrell <enrellsa10@proton.me>
2023-11-01 01:09:01 -0300

    final version

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Update source list
-sudo apt-get update
 sudo cp /etc/apt/sources.list /etc/apt/source.list.bak
 
 wget https://raw.githubusercontent.com/GLUA-UA/UbuntuMirrorsScript/master/glua_mirrors_ubuntu.sh
@@ -9,9 +8,6 @@ sudo bash glua_mirrors_ubuntu.sh
 sudo apt-get update
 
 # Install Docker and Docker Compose
-
-sudo systemctl disable --now docker.service docker.socket
-systemctl --user disable --now docker.service
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
@@ -25,7 +21,22 @@ echo "Running rootless-install.sh..."
 ./rootless-install.sh
 
 # Check the exit status of the rootless-install.sh script
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
+    echo "rootless-install.sh successful. Enabling and starting Docker service..."
+    systemctl --user enable docker.service --now
+
+    if [ $? -eq 0 ]; then
+        echo "Docker service enabled successfully. Adding environment variables to .bashrc..."
+
+        # Adiciona as variáveis de ambiente ao final do arquivo .bashrc
+        echo "export PATH=/home/enrell/bin:\$PATH" >> ~/.bashrc
+        echo "export DOCKER_HOST=unix:///run/user/1000/docker.sock" >> ~/.bashrc
+
+        echo "Environment variables added to .bashrc."
+    else
+        echo "Failed to enable Docker service. Environment variables not added to .bashrc."
+    fi
+else
     # If it returns an error, install uidmap and try to activate ip_tables
     echo "Installing uidmap..."
     sudo apt-get install -y uidmap
@@ -41,8 +52,29 @@ if [ $? -ne 0 ]; then
             # Tenta executar o rootless-install.sh novamente
             echo "Trying to run rootless-install.sh again..."
             ./rootless-install.sh
+
+            # Verifica se a segunda execução do rootless-install.sh foi bem-sucedida
+            if [ $? -eq 0 ]; then
+                echo "rootless-install.sh successful. Enabling and starting Docker service..."
+                systemctl --user enable docker.service --now
+
+                if [ $? -eq 0 ]; then
+                    echo "Docker service enabled successfully. Adding environment variables to .bashrc..."
+
+                    # Adiciona as variáveis de ambiente ao final do arquivo .bashrc
+                    echo "export PATH=/home/enrell/bin:\$PATH" >> ~/.bashrc
+                    echo "export DOCKER_HOST=unix:///run/user/1000/docker.sock" >> ~/.bashrc
+
+                    echo "Environment variables added to .bashrc."
+                else
+                    echo "Failed to enable Docker service. Environment variables not added to .bashrc."
+                fi
+            else
+                echo "rootless-install.sh failed. Docker service not enabled."
+            fi
         fi
     else
         echo "Error: Failed to install uidmap. Check your network and try again."
     fi
 fi
+
